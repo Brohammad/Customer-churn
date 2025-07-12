a406bcc6945aff1755d4c6440a7df2e1267156c7
brohammad <aabid_b210852ee@nitc.ac.in>
2025-07-12 16:33:25 +0530

    loaded dataset

--- Model.py
+++ Model.py
@@ -1,1 +1,12 @@
-import pandas as 
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error,balanced_accuracy_score,roc_auc_score,make_scorer, confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+
+from sklearn.model_selection import GridSearchCV
+
+df=pd.read_csv("Teleco_Customer_Churn.csv")
+print(df.head())
